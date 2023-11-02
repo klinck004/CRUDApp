731da36d259c51353d6ff9b2d73d0ca452290709
klinck004 <keenan.linck@ontariotechu.net>
2023-11-02 18:13:58 -0400

    Continued header/footer development

--- app.js
+++ app.js
@@ -20,9 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', testRouter);
 app.use('/users', usersRouter);
-app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
