34dee3e123e650bae6b27e16c384469d554c01da
klinck004 <keenan.linck@ontariotechu.net>
2023-11-02 21:50:56 -0400

    Design and routing changes
    - Changed routes to solely originate from ./routes/index
    - Finalized home page
    - Created About Me page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '' });
+  res.render('bootstrap.ejs', { title: 'Keenan Linck' });
+});
+
+router.get('/aboutme', function(req, res, next) {
+  res.render('aboutme.ejs', { title: 'About Me - Keenan Linck' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('bootstrap.ejs', { title: 'Projects - Keenan Linck' });
+});
+
+router.get('/contactme', function(req, res, next) {
+  res.render('bootstrap.ejs', { title: 'Contact Me - Keenan Linck' });
 });
 
 module.exports = router;
