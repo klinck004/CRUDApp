3aad5a3db0cf0a42b66c9f850c9bf8ceae05bf11
klinck004 <keenan.linck@ontariotechu.net>
2023-11-14 18:46:07 -0500

    Fully implemented CRUD foundation
    - Finished the implementation of Create-Read-Update-Delete functionality
    - CRUD operations work properly for the test database type (Book list)
    - The operations will be changed to fit the assignment's goal and to integrate with the user interface

--- server/routes/Bio_books.js
+++ server/routes/Bio_books.js
@@ -1,10 +1,9 @@
+/*CRUD route file*/
 var express = require('express');
 let mongoose = require('mongoose');
 var router = express.Router();
 
-
 let Book = require('../models/Bio_books');
-
 
 /*Read*/
 router.get('/', async (req,res,next)=>{ 
@@ -21,17 +20,16 @@ router.get('/', async (req,res,next)=>{
        });
     }
  });
+
 /*Create */
 /*Get route*/
 router.get('/add', async (req,res,next)=>{ 
     res.render('CRUD/add', {
         title: 'Add', 
-        test: 'test',
     })
 });
 /*Post route*/
 router.post('/add', async (req,res,next)=>{ 
-    console.log("NEW");
     let newBook = Book({
         "name":req.body.name,
         "author":req.body.author,
@@ -39,7 +37,6 @@ router.post('/add', async (req,res,next)=>{
         "description":req.body.description,
         "price":req.body.price,
     });
-    console.log("log");
     console.log(newBook);
     Book.create(newBook)
         .then(() => {
@@ -52,24 +49,64 @@ router.post('/add', async (req,res,next)=>{
         });
     });
 
-
-
-
-
 /*Update*/
 /*Get route*/
 router.get('/edit/:id', async (req,res,next)=>{ 
-});
+    try{
+        let id = req.params.id;
+        const bookToEdit = await Book.findById(id)
+        res.render('CRUD/edit', {
+           title: 'Edit', 
+           id: id,
+           bookToEdit: bookToEdit,
+        });
+        console.log("Book to edit");
+        console.log(bookToEdit);
+     }catch(err){
+        console.error(err);
+        res.render('CRUD/edit', {
+           error: 'Error on server'
+        });
+     }
+    }
+);
 /*Post route*/
 router.post('/edit/:id', async (req,res,next)=>{ 
+    let id=req.params.id;
+    let updateBook = Book({
+        "_id": id,
+        "name":req.body.name,
+        "author":req.body.author,
+        "published":req.body.published,
+        "description":req.body.description,
+        "price":req.body.price,
+    })
+    console.log("Post");
+    console.log(updateBook);
+    Book.updateOne({_id: id}, updateBook)
+    .then(() => {
+        res.redirect('/book');
+    })
+    .catch((err) => {
+        console.log(err);
+        console.log("Edit error! :(");
+        res.send(err);
+    });
 });
 
 /*Delete*/
 /*Get route*/
 router.get('/delete/:id', async (req,res,next)=>{ 
+    let id = req.params.id;
+    Book.deleteOne({_id: id}).then(() => {
+        res.redirect('/book');
+    })
+    .catch((err) => {
+        console.log(err);
+        console.log("Delete error! :(");
+        res.send(err);
+    })
 });
-/*Post route*/
-router.post('/delete/:id', async (req,res,next)=>{ 
-});
+
 
 module.exports = router; /*Please do not delete me :(*/
